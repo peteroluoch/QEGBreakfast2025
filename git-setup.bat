74a49656a2ed5db8ddeb77772cc72c565d70607c
Your Name <user@example.com>
2025-05-09 23:08:58 +0300

    Initial commit: Queen Esthers' Generation 2025 Economic Empowerment Breakfast Meeting website

---
+++ git-setup.bat
@@ -0,0 +1,8 @@
+@echo off
+cd C:\Users\LENOVO\Documents\augment-projects\QEGEconomicEmpowerment
+git init
+git add .
+git commit -m "Initial commit: Queen Esthers' Generation 2025 Economic Empowerment Breakfast Meeting website"
+git remote add origin https://github.com/peteroluoch/QEGBreakfast2025.git
+git push -u origin main
+echo Git setup complete. Check the output for any errors.
